bf97c7a02d1900edcbd04e2416b5f03d24076280
tpeplow <tom.peplow@googlemail.com>
2012-01-16 08:08:27 +0000

    Added some tests for wildcard matching

---
+++ src/NoSln.Specifications/IO/WildcardMatchingSpecifications.cs
@@ -0,0 +1,32 @@
+﻿using Machine.Specifications;
+
+namespace NoSln.Specifications.IO
+{
+    [Subject(typeof(WildcardMatcher))]
+    public class when_matching_a_wildcard_with_a_filepath
+    {
+        static WildcardMatcher wildcardMatcher;
+        Establish context = () => 
+                                {
+                                    wildcardMatcher = new WildcardMatcher();
+                                };
+
+        It should_match_full_path = () => wildcardMatcher.IsMatch("c:\\afile.txt", "c:\\afile.txt").ShouldBeTrue();
+        
+        It should_match_if_path_ends_with_wildcard = () => wildcardMatcher.IsMatch("c:\\afile.txt", "afile.txt").ShouldBeTrue();
+
+        It should_match_all_sub_folders_if_wildcard_has_trailing_slash = () => wildcardMatcher.IsMatch("c:\\somefolder\\somefile.txt", "somefolder\\").ShouldBeTrue();
+
+        It should_match_if_any_file_matches_wildcard_that_uses_an_asterix = () => wildcardMatcher.IsMatch("c:\\somefolder\\dave.dll", "*.dll").ShouldBeTrue();
+
+        It should_match_if_a_question_mark_is_used_to_denote_any_character = () => wildcardMatcher.IsMatch("c:\\afile.txt", "a?ile.txt");
+    }
+
+    public class WildcardMatcher
+    {
+        public bool IsMatch(string filePath, string wildcard)
+        {
+            return false;
+        }
+    }
+}
