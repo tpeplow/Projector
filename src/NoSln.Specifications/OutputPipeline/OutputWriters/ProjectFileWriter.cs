d814c165d512fb7452db7bcce3da2487c8d14f79
tpeplow <tom.peplow@googlemail.com>
2012-01-20 18:29:14 +0000

    Added file writer

---
+++ src/NoSln.Specifications/OutputPipeline/OutputWriters/ProjectFileWriter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Machine.Specifications;
+using NoSln.Model.Output;
+using NoSln.OutputPipeline.OutputWriters;
+
+namespace NoSln.Specifications.OutputPipeline.OutputWriters
+{
+    [Subject(typeof(ProjectFileWriter))]
+    public class when_writing_a_project_file : when_writing_item_group<IEnumerable<ProjectFile>>
+    {
+        Establish context = () => 
+        {
+            writer = new ProjectFileWriter();
+            part = new[]
+            {
+                new ProjectFile
+                {
+                    RelativePath = "afile.cs"
+                }
+            };
+        };
+        
+        It should_write_the_compile_element = () => element.Element("Compile").ShouldNotBeNull();
+
+        It should_write_include_attribute = () => element.Element("Compile").Attribute("Include").Value.ShouldEqual("afile.cs");
+    }
+}
---
+++ src/NoSln/OutputPipeline/OutputWriters/ProjectFileWriter.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using NoSln.Model.Output;
+
+namespace NoSln.OutputPipeline.OutputWriters
+{
+    public class ProjectFileWriter : ItemGroupWriter<IEnumerable<ProjectFile>>
+    {
+        protected override IEnumerable<XElement> GetItems(IEnumerable<ProjectFile> part)
+        {
+            return part.Select(x => new XElement("Compile", new XAttribute("Include", x.RelativePath)));
+        }
+    }
+}
