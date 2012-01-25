f795fbc838274137d6233627190392e022437350
Tom Peplow <tom.peplow@googlemail.com>
2012-01-25 18:24:06 +0000

    Added the idea of conventions.  Moved the lib reference path step to be a convention

---
+++ src/Projector/OutputPipeline/Conventions/DefaultConventions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Projector.OutputPipeline.Conventions
+{
+    public static class DefaultConventions
+    {
+        public static IEnumerable<IOutputConvention> Create()
+        {
+            return new IOutputConvention[]
+            {
+                new LibHintPathGeneratorStep()
+            };
+        }
+    }
+}
