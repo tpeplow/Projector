1afdc1dab0e7e4671d60b4e211abf4212960e191
Tom Peplow <tom.peplow@googlemail.com>
2012-01-31 10:14:39 +0000

    put full path to msbuild

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-msbuild src\Projector.sln
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild src\Projector.sln
 lib\mspec-clr4.exe src\Projector.Specifications\bin\Debug\Projector.Specifications.dll
+break
